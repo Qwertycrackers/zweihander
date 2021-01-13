f1838319dbee7465b9c673867f53f1b0ec6bd052
Greg Gerardot <magpiegerardot@gmail.com>
2021-01-13 15:32:58 -0500

    change character generator

--- ZweihanderCharacterGen.java
+++ ZweihanderCharacterGen.java
@@ -16,6 +16,7 @@ public class ZweihanderCharacterGen
 		String[] skillNames = {"com","bra","agi","per","wil","int","fel"};
 		int length = skills.length;
 		Scanner input = new Scanner(System.in);
+		int timesRerolled=0;
 
 		//Get stat that it needs to be above
 		System.out.print("Preferred stat(First 3 letters) - ");
@@ -23,35 +24,36 @@ public class ZweihanderCharacterGen
 		//Get number it needs to be above
 		System.out.print("What is the min value for this? - ");
 		preferStatMin = input.nextInt();
+		if(preferStatMin>55)
+			System.out.println("You're an idiot and need to type a smaller number");
 
 		//Create Proper stats
-
+		if(preferStatMin<=55)
+		{
 		for(int i=0;i<length;i++)
 		{
 			skills[i]=STARTSTAT+Roll3D10();
-		}
-		for(int g=0;g<length;g++)
-		{
-			//System.out.println("I made it this far");
-			if(preferStat.equals(skillNames[g]))
+			if(preferStat.equals(skillNames[i]))
 			{
 				System.out.println("I found the right skill");
 				while(goodStats==false)
 				{
-					if(skills[g]>= preferStatMin)
+					if(skills[i]>= preferStatMin)
 					{
 						goodStats=true;
 					}
 					else
 					{
-						skills[g]+=5;
-						System.out.println("I added 5");
+					timesRerolled++;
+					skills[i]=STARTSTAT+Roll3D10();
 					}
 				}
 			}
 		}
+		}
 
 		//Tell them to user
+		System.out.println("I rerolled the skill "+timesRerolled+" times.");
 		for(int i=0;i<length;i++)
 		{
 			System.out.println(skillNames[i]+" "+ skills[i]);
